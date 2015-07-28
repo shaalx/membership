a24a9424fb6fbe844aa8d2f6dc5b8d151670714d
shaalx <wisdomswing@gmail.com>
2015-07-28 15:23:36 +0800

    yml

---
+++ tag.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+git add -A
+read tags
+git commit -m $tags
+git push origin master:master
+git tag -a $tags -m $tags
+git push origin --tag $tags
+sleep 30
+git push origin --tag :$tags
+git tag -d $tags
