2484bca896502e1c99e296f22107fa7170832a7b
shaalx <wisdomswing@gmail.com>
2015-07-28 22:49:23 +0800

    v1

--- tag.sh
+++ tag.sh
@@ -3,7 +3,7 @@ git add -A
 read tags
 git commit -m $tags
 #git push origin master:master
-git push origin devm:devm
+# git push origin devm:devm
 git tag -a $tags -m $tags
 git push origin --tag $tags
 sleep 50
