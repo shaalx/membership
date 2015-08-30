b08664e0ad5f238168140aceb700f268658bd23e
shaalx <wisdomswing@gmail.com>
2015-08-30 12:37:16 +0800

    lfu

--- main.go
+++ main.go
@@ -4,22 +4,35 @@ import (
 	"bufio"
 	"encoding/json"
 	"fmt"
+	"github.com/shaalx/membership/u"
 	"html/template"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"time"
 )
+
+type Bookmark struct {
+	Title    string `json:"title"`
+	Official string `json:"official"`
+	Bgpic    string `json:"bgpic"`
+	Site     string `json:"site"`
+	Remark   string `json:"remark"`
+}
+
+var cache *u.LFUCache
 
 func readFile(name string) []byte {
 	f, _ := os.OpenFile(name, os.O_RDONLY, 0064)
 	defer f.Close()
 	rd := bufio.NewReader(f)
 	b, _ := ioutil.ReadAll(rd)
+	fmt.Println(string(b))
 	return b
 }
 
-func unmarshal(b []byte) []interface{} {
-	var v []interface{}
+func unmarshal(b []byte) []*Bookmark {
+	var v []*Bookmark
 	err := json.Unmarshal(b, &v)
 	if nil != err {
 		fmt.Println(err)
@@ -29,13 +42,16 @@ func unmarshal(b []byte) []interface{} {
 }
 
 func main() {
+	go updateBookmarks(time.Second)
 	http.HandleFunc("/", bookmark)
+	http.HandleFunc("/lfu", lfu)
 	http.ListenAndServe(":80", nil)
 }
 
 var (
-	t *template.Template
-	v []interface{}
+	t      *template.Template
+	v      []*Bookmark
+	update chan bool
 )
 
 func get(_url string) []byte {
@@ -45,13 +61,48 @@ func get(_url string) []byte {
 }
 
 func init() {
+	update = make(chan bool, 10)
 	t, _ = template.New("bookmark.html").ParseFiles("bookmark.html")
 	// b := readFile("user.md")
 	b := get("http://7xku3c.com1.z0.glb.clouddn.com/bookmark.md")
 	v = unmarshal(b)
+	cache = u.NewLFUCache(4)
+	for i := len(v) - 1; i >= 0; i-- {
+		cache.Set(v[i].Title, v[i])
+	}
+}
+
+func bookmarkInCache() []*Bookmark {
+	vals := cache.Vals()
+	ret := make([]*Bookmark, len(vals))
+	for i, it := range vals {
+		bok := it.V.(*Bookmark)
+		ret[i] = bok
+	}
+	return ret
+}
+
+func updateBookmarks(d time.Duration) {
+	ticker := time.NewTicker(d)
+	for {
+		<-ticker.C
+		v = bookmarkInCache()
+		<-update
+	}
 }
 
 func bookmark(rw http.ResponseWriter, req *http.Request) {
 	fmt.Printf("%s  ", req.RemoteAddr)
 	t.Execute(rw, v)
+}
+
+func lfu(rw http.ResponseWriter, req *http.Request) {
+	if req.Method != "POST" {
+		return
+	}
+	req.ParseForm()
+	title := req.Form.Get("title")
+	fmt.Printf("[ %s %s ]", req.RemoteAddr, title)
+	cache.Get(title)
+	update <- true
 }
---
+++ u/lfu.go
@@ -0,0 +1,224 @@
+package u
+
+import (
+	"fmt"
+	"sync"
+)
+
+type LFUCache struct {
+	last, latest *LFU
+	size         int
+	v            map[string]*LFU
+	remv         map[string]*LFU
+	sync.RWMutex
+}
+
+func NewLFUCache(size int) *LFUCache {
+	if size <= 1 {
+		panic("it makes no sense.")
+	}
+	return &LFUCache{
+		size: size,
+		v:    make(map[string]*LFU),
+		remv: make(map[string]*LFU),
+	}
+}
+
+func (c *LFUCache) Vals() []*LFU {
+	ret := make([]*LFU, 0, c.size)
+	for lfu := c.latest; lfu != nil; lfu = lfu.next {
+		ret = append(ret, lfu)
+	}
+	return ret
+}
+
+func (c *LFUCache) Display() {
+	c.RLock()
+	defer c.RUnlock()
+	first := true
+	i := 0
+	for lfu := c.latest; lfu != nil; lfu = lfu.next {
+		i++
+		if i > 10 {
+			break
+		}
+		if lfu == c.latest && !first {
+			break
+		}
+		first = false
+		fmt.Print(lfu.String(), " //// ")
+	}
+	fmt.Println()
+}
+
+func (c *LFUCache) Latest() *LFU {
+	c.RLock()
+	defer c.RUnlock()
+	return c.latest
+}
+
+func (c *LFUCache) Last() *LFU {
+	c.RLock()
+	defer c.RUnlock()
+	return c.last
+}
+
+type LFU struct {
+	Key       string
+	V         interface{}
+	N         int
+	pre, next *LFU
+}
+
+func NewLFU(key string, v interface{}) *LFU {
+	return &LFU{
+		Key: key,
+		V:   v,
+		N:   1,
+	}
+}
+
+func (l *LFU) String() string {
+	return fmt.Sprintf("<%d times> %s:%v", l.N, l.Key, l.V)
+}
+
+func (c *LFUCache) Get(key string) (cur *LFU) {
+	c.Lock()
+	defer c.Unlock()
+	cur, exist := c.v[key]
+	if !exist {
+		out_cur, out_exist := c.remv[key]
+		if out_exist {
+			c.revoke(key)
+			return out_cur
+		}
+		return nil
+	}
+	cur.N++
+	if cur == c.latest {
+		return
+	}
+	c.move(cur, cur.pre)
+	return
+}
+
+func (c *LFUCache) Set(key string, v interface{}) (cur *LFU) {
+	c.Lock()
+	defer c.Unlock()
+	cur, exist := c.v[key]
+	// fmt.Println(exist, key, cur, c.v)
+	if !exist {
+		cur_remv, ok := c.remv[key]
+		if ok {
+			c.revoke(key)
+			return cur_remv
+		}
+		// remove the last node
+		if len(c.v) >= c.size {
+			c.remove()
+		}
+		cur = NewLFU(key, v)
+		c.v[key] = cur
+		// cur will be the first node
+		if len(c.v) <= 1 {
+			c.last = cur
+			c.latest = cur
+			return
+		}
+		dst := c.loc(cur.N, c.last)
+		c.in(dst, cur)
+		return
+	}
+	cur.N++
+	if cur == c.latest {
+		return
+	}
+	c.move(cur, cur.pre)
+	return
+}
+
+// remove the last node from dequeue to remv
+func (c *LFUCache) remove() {
+	remv := c.last
+	c.last = remv.pre
+	remv.pre = nil
+	c.last.next = nil
+	c.remv[remv.Key] = remv
+	delete(c.v, remv.Key)
+}
+
+// location, from is the node before n
+func (c *LFUCache) loc(n int, from *LFU) *LFU {
+	var tmp *LFU
+	for tmp = from; tmp != nil && tmp.N <= n; tmp = tmp.pre {
+	}
+	return tmp
+}
+
+// cur is outside of the dequeue
+func (c *LFUCache) revoke(key string) {
+	cur, ok := c.remv[key]
+	if !ok {
+		return
+	}
+	c.remove()
+	delete(c.remv, key)
+	cur.N++
+	dst := c.loc(cur.N, c.last)
+	c.in(dst, cur)
+	c.v[key] = cur
+}
+
+// cur is inside of the dequeue, from is the node before cur
+func (c *LFUCache) move(cur, from *LFU) {
+	if cur == c.latest || from == nil {
+		return
+	}
+	dst := c.loc(cur.N, from)
+
+	// location is currect now
+	if dst == from {
+		return
+	}
+	c.out(cur)
+	c.in(dst, cur)
+	return
+}
+
+// insert cur after dst
+func (c *LFUCache) in(dst, cur *LFU) {
+	// cur will be the latest node
+	if dst == nil {
+		cur.next = c.latest
+		c.latest.pre = cur
+		c.latest = cur
+		return
+	}
+	// dst is the last node, then cur will be the last node
+	if c.last == dst {
+		cur.pre = c.last
+		c.last.next = cur
+		c.last = cur
+		return
+	}
+	// insert cur after dst
+	cur.next = dst.next
+	cur.pre = dst
+	cur.next.pre = cur
+	dst.next = cur
+}
+
+// pick cur outof the dequeue
+func (c *LFUCache) out(cur *LFU) {
+	// if cur == nil || cur.pre == nil {
+	// 	panic("cur must not be nil")
+	// }
+	if c.last != cur {
+		cur.next.pre = cur.pre
+	} else {
+		c.last = cur.pre
+		c.last.next = nil
+	}
+	cur.pre.next = cur.next
+	cur.pre = nil
+}
