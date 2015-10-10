6d17af232b0992d02706322f9d8f45269045ae66
shaalx <wisdomswing@gmail.com>
2015-10-10 11:14:06 +0800

    auto

--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import (
 	"bufio"
 	"encoding/json"
 	"fmt"
-	"github.com/shaalx/membership/u"
+	. "github.com/everfore/oauth/oauth2"
+	"github.com/shaalx/leetcode/lfu2"
 	"html/template"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"time"
@@ -21,7 +23,40 @@ type Bookmark struct {
 	N        int    `json:"n"`
 }
 
-var cache *u.LFUCache
+var (
+	t      *template.Template
+	hacker *template.Template
+	v      []*Bookmark
+	update chan bool
+	cache  *lfu2.LFUCache
+	OA     *OAGithub
+)
+
+func init() {
+	update = make(chan bool, 10)
+	hacker, _ = template.New("hacker.html").ParseFiles("hacker.html")
+	t, _ = template.New("bookmark.html").ParseFiles("bookmark.html")
+	// b := readFile("bookmark.md")
+	b := get("http://7xku3c.com1.z0.glb.clouddn.com/bookmark.md")
+	v = unmarshal(b)
+	cache = lfu2.NewLFUCache(len(v))
+	for i := len(v) - 1; i >= 0; i-- {
+		cache.Set(v[i].Title, v[i])
+	}
+	update <- true
+	OA = NewOAGithub("8ba2991113e68b4805c1", "b551e8a640d53904d82f95ae0d84915ba4dc0571", "user", "http://bookmark.daoapp.io/callback")
+}
+
+func main() {
+	go updateBookmarks(time.Second)
+	go flushBookmarks(time.Hour * 24 * 30)
+	http.HandleFunc("/", bookmark)
+	http.HandleFunc("/hacker", hackerHandler)
+	http.HandleFunc("/lfu", lfu)
+	http.HandleFunc("/signin", signin)
+	http.HandleFunc("/callback", callback)
+	http.ListenAndServe(":80", nil)
+}
 
 func readFile(name string) []byte {
 	f, _ := os.OpenFile(name, os.O_RDONLY, 0064)
@@ -42,38 +77,12 @@ func unmarshal(b []byte) []*Bookmark {
 	return v
 }
 
-func main() {
-	go updateBookmarks(time.Second)
-	go flushBookmarks(time.Hour * 24 * 30)
-	http.HandleFunc("/", bookmark)
-	http.HandleFunc("/lfu", lfu)
-	http.ListenAndServe(":80", nil)
-}
-
-var (
-	t      *template.Template
-	v      []*Bookmark
-	update chan bool
-)
-
 func get(_url string) []byte {
 	resp, _ := http.Get(_url)
 	b, _ := ioutil.ReadAll(resp.Body)
 	return b
 }
 
-func init() {
-	update = make(chan bool, 10)
-	t, _ = template.New("bookmark.html").ParseFiles("bookmark.html")
-	// b := readFile("bookmark.md")
-	b := get("http://7xku3c.com1.z0.glb.clouddn.com/bookmark.md")
-	v = unmarshal(b)
-	cache = u.NewLFUCache(len(v))
-	for i := len(v) - 1; i >= 0; i-- {
-		cache.Set(v[i].Title, v[i])
-	}
-	update <- true
-}
 func flushBookmarks(d time.Duration) {
 	ticker := time.NewTicker(d)
 	for {
@@ -104,6 +113,11 @@ func bookmark(rw http.ResponseWriter, req *http.Request) {
 	t.Execute(rw, v)
 }
 
+func hackerHandler(rw http.ResponseWriter, req *http.Request) {
+	fmt.Printf("%s  ", req.RemoteAddr)
+	hacker.Execute(rw, nil)
+}
+
 func lfu(rw http.ResponseWriter, req *http.Request) {
 	if req.Method != "POST" {
 		return
@@ -113,4 +127,31 @@ func lfu(rw http.ResponseWriter, req *http.Request) {
 	fmt.Printf("[ %s %s ]", req.RemoteAddr, title)
 	cache.Get(title)
 	update <- true
+}
+
+func signin(rw http.ResponseWriter, req *http.Request) {
+	http.Redirect(rw, req, OA.AuthURL(), 302)
+}
+
+func callback(rw http.ResponseWriter, req *http.Request) {
+	log.Printf("%s\n", req.RemoteAddr)
+	b, err := OA.NextStep(req)
+	if nil != err {
+		rw.Write([]byte(err.Error()))
+		return
+	}
+	var ret map[string]interface{}
+	err = json.Unmarshal(b, &ret)
+	if nil == err {
+		t := template.New("oauth.html")
+		t, err := t.ParseFiles("oauth.html")
+		if nil != err {
+			return
+		}
+		now := time.Now().String()
+		ret["now"] = now
+		t.Execute(rw, ret)
+	} else {
+		rw.Write([]byte(err.Error()))
+	}
 }
